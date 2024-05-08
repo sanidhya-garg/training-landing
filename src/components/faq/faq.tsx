63204ef7c5dfea797dce4b6850322f6634135085
Arush Bansal <arush4bansal@gmail.com>
2024-05-08 07:10:10 +0530

    quick fix

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
